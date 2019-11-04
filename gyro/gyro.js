704dc5eea0eecf3fe8a68cc4588c540da7fab0e7
cristilupsa <56259352+cristilupsa@users.noreply.github.com>
2019-11-04 10:46:47 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,10 +1,10 @@
-document.getElementsById("id_logic_version").innerHTML="Logic = 2019.11.4.0";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.4.0";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 
-funciton on_gyro_data_uab(e)
+function on_gyro_data_uab(e)
 {
-  docment.getElementById("id_alpha").innerHTML = e.alpha;
-  docment.getElementById("id_beta").innerHTML = e.beta;
-  docment.getElementById("id_gamma").innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
 }
