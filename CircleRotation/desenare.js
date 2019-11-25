f16aec0a77ecd784bb18d67ca668b231cccbc2c2
cristilupsa <56259352+cristilupsa@users.noreply.github.com>
2019-11-25 13:17:57 +0200

    Add files via upload

--- CircleRotation/desenare.js
+++ CircleRotation/desenare.js
@@ -1,10 +1,11 @@
-document.getElementById("id_logic").innerHTML="Logic = 2019.11.25.5";
+document.getElementById("id_logic").innerHTML="Logic = 2019.11.25.6";
 document.getElementById("id_start").addEventListener("click",start);
 document.getElementById("id_stop").addEventListener("click",stop);
 
 var timer_id;
 var unghi = {};
 unghi.valoare = 0;
+var muncitor = null;
 	
 function desenare(unghi)
 {
@@ -32,15 +33,23 @@ function start()
 	document.getElementById("id_start").disabled = true;
 	document.getElementById("id_stop").disabled = false;
 	timer_id = setInterval(desenare, 20, unghi);
-	var muncitor = new Worker("prime.js");
+	
+	if(muncitor == null)
+	{
+	muncitor = new Worker("prime.js");
 	muncitor.onmessage = function(e){
 		document.getElementById("id_prime").innerHTML = e.data;
 	}
+	}
+	else
+		muncitor.postMessage("start");
 }
+
 
 function stop()
 {
 	document.getElementById("id_start").disabled = false;
 	document.getElementById("id_stop").disabled = true;
 	clearInterval(timer_id);
+	muncitor.postMessage("stop");
 }
